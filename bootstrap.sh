777b47771ddcd81ffdd80918cc37bdba9c3e92b9
Alexandre Côté <a.cote@2kloc.com>
2018-05-09 00:12:57 -0400

    Fix some issue with the script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,15 @@
 #!/bin/sh 
 
+# Installing Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+# Generate SSH key
+ssh-keygen -o -a 100 -t ed25519
+
 cd ~
 
 ln -sf ~/.dotfiles/Brewfile ~/Brewfile
-
 brew bundle
 
 # ZSH
@@ -11,6 +17,7 @@ ln -sf ~/.dotfiles/zsh ~/.zsh
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 
 chsh -s /bin/zsh
+zplug install
 
 # Git
 ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
@@ -35,4 +42,4 @@ ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
 # GPG
 ln -sf ~/.dotfiles/gnupg ~/.gnupg
 
-./.macos
+./.dotfiles/.macos
