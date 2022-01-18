729692f7653465f32c048fb0f4b75333f72736e2
Alexandre Côté <alex.cote@shopify.com>
2022-01-18 10:48:31 -0500

    Create shortcuts

--- zsh/config/function.zsh
+++ zsh/config/function.zsh
@@ -18,3 +18,30 @@ function ss() {
         echo "$1 is not supported"
     fi
 }
+
+base() {
+    local regex=/Shopify/(.+)/?
+    local command="$1"
+    local current_path=$(pwd) > /dev/null 2>&1
+    if [[ $current_path =~ $regex ]]; then
+      eval "${command} shopify--${match[1]}"
+    else
+      echo "Please run this script from a Shopify project"
+    fi
+}
+
+start() {
+  base "iso procs start"
+}
+
+stop() {
+  base "iso procs stop"
+}
+
+restart() {
+  base "iso procs restart"
+}
+
+update() {
+  base "/usr/bin/update"
+}
