7e7a770456503be10c7f1221319496842c1cbf64
Alexandre Côté <alex.cote@shopify.com>
2024-05-13 11:06:43 -0400

    Adapt for work

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,6 @@
 # Installing Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Generate SSH key
-ssh-keygen -o -a 100 -t ed25519
-
 cd ~
 
 ln -sf ~/.dotfiles/Brewfile ~/Brewfile
@@ -32,11 +29,6 @@ ln -sf ~/.tmux/tmux-inactive.conf ~/.tmux-inactive.conf
 # Misc
 ln -sf ~/.dotfiles/misc/inputrc ~/.inputrc
 
-# Alacritty
-mkdir -p ~/.config
-rm -rf ~/.config/alacritty
-ln -sfn ~/.dotfiles/alacritty/ ~/.config/alacritty
-
 # iterm
 plutil -convert binary1 ~/.dotfiles/iterm/com.googlecode.iterm2.xml -o ~/.dotfiles/iterm/com.googlecode.iterm2.plist
 ln -sf ~/.dotfiles/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
@@ -46,9 +38,6 @@ ln -sfn ~/.dotfiles/karabiner ~/.config/karabiner
 
 # Irb
 ln -sf ~/.dotfiles/irc/irbrc ~/.irbrc
-
-# Elixir
-ln -sf ~/.dotfiles/iex/iex.exs ~/.iex.exs
 
 # Vim
 ln -sfn ~/.dotfiles/vim ~/.vim
--- iex/iex.exs
+++
@@ -1,3 +0,0 @@
-IEx.configure(colors: [eval_result: [:red, :bright]])
-IEx.configure(inspect: [limit: 10])
-IEx.configure(history_size: 100)
